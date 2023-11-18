6b330f45447809c966b4582ca036480cc454f1cb
ingridpaixao <66692941+ingridpaixao@users.noreply.github.com>
2023-11-18 14:17:43 -0800

    Update main.js
    Updating comments

--- main.js
+++ main.js
@@ -23,15 +23,12 @@ const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
 
 
-// Add your functions below:
 function validateCred(array){
     let checkDigit = array[array.length - 1];
     let accumulator = checkDigit;
-    //console.log(checkDigit);
     if(array.length % 2 == 0){
         for(let i = array.length-2; i >= 0; i--){
             let element = array[i];
-            //console.log(element);
             if( i%2 === 0 ){
                 element *= 2;
             }
@@ -39,7 +36,6 @@ function validateCred(array){
                 element -= 9;
             }
             accumulator += element;
-            //console.log(accumulator);
         }
     }
     else {
